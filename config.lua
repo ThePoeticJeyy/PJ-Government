c606e2f1ee4d0d245f666ae13cdbf56f6681c17e
ThePoeticJeyy <161110642+ThePoeticJeyy@users.noreply.github.com>
2026-02-13 05:41:52 -0500

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,26 @@
+Config = {}
+
+Config.Jobs = {
+    mayor = "mayor",
+    doj = "doj",
+    judge = "judge"
+}
+
+Config.Societies = {
+    state = "society_state",
+    doj = "society_doj"
+}
+
+Config.Webhooks = {
+    cases = "",
+    warrants = "",
+    taxes = "",
+    elections = ""
+}
+
+Config.Targets = {
+    JurySignup = vector3(-548.4,-197.1,38.2),
+    JuryVote = vector3(-552.0,-200.3,38.2),
+    JudgePanel = vector3(-555.0,-203.0,38.2),
+    MayorPanel = vector3(-540.2,-198.0,38.2)
+}
